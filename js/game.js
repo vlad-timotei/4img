81801cfb436f05ede417a47d7c7ab4627808b2fa
Vlad Timotei <vlad-timotei@users.noreply.github.com>
2020-07-26 10:25:22 +0300

    Check old data optimisation

--- js/game.js
+++ js/game.js
@@ -589,19 +589,23 @@ function setLS(cname, cvalue) {
 }
 
 function newDomain(data) {
-	if(getLS(game)==0){
     var oldData=JSON.parse(data);
 	setval(game,oldData.level);
 	setval(game+"_score",oldData.score);
 	setval(game+"_mode",oldData.mode);
 	check_player();
 	console.log("Am primit "+data);
+}
+
+function check_old_data() {
+	if(getLS(game)==0){
+	window.addEventListener("message", function(event){newDomain(event.data);});
+	document.getElementById("transfer-player-data").src = "https://raduanastase.com/fb-api/_extra/4img/TransferPlayerData.php?game=4img1word_19052020F&ver=062020";
 	}
 }
 
 $(document).ready(function() { 
-    window.addEventListener("message", function(event){newDomain(event.data);});
-	document.getElementById("transfer-player-data").src = "https://raduanastase.com/fb-api/_extra/4img/TransferPlayerData.php?game=4img1word_19052020F&ver=062020";
+    check_old_data();
     check_player();
     $("#mode").change(change_mode);
 });
