f766ea096386466953eaf7aab0fca24333928d27
Vlad Timotei <vlad-timotei@users.noreply.github.com>
2020-07-26 11:51:00 +0300

    Preload parseInt

--- js/game.js
+++ js/game.js
@@ -535,7 +535,7 @@ function preload_current_images(){
 
 function preload_next_images(){
 	if(player.level<(levels.length)){
-	var nextlevel = levels[parseInt(player.level)+1].split('|', 3);
+	var nextlevel = levels[parseInt(parseInt(player.level)+1)].split('|', 3);
 	var imgs_urls=nextlevel[1].split(',', 4);
 	preload_imgs(imgs_urls);
 	}
